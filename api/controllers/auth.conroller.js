ce9993d12d92ffe6741fb28af15bf180668e265c
SairajKulkarni <kulkarnisairaj.967@gmail.com>
2024-09-06 15:25:38 +0530

    Create Sign in API route

--- api/controllers/auth.conroller.js
+++ api/controllers/auth.conroller.js
@@ -1,30 +1,52 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
-    // Extracting fields from request body
     const { username, email, password } = req.body;
 
-    // Validate input data
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required." });
     }
 
-    // Hash the password
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    // Create a new user instance
     const newUser = new User({ username, email, password: hashedPassword });
 
-    // Save the user to the database
     await newUser.save();
 
-    // Respond with a success message
     res.status(201).json({ message: "User created successfully" });
   } catch (err) {
-    // Use the error handler or directly pass the error to the next middleware
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Invalid credentials"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
